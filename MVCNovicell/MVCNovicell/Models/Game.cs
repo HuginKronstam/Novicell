46b8ac0c0a1a47679acc708492cf178986bf8a58
snigern <andreas.kronstam@gmail.com>
2019-02-28 20:45:08 +0100

    Har tilføjet dynamisk skift af sprog mellem dansk og engelsk vha resource library. Gør alle strings i programmet dynamiske hvis jeg har tid.

--- MVCNovicell/MVCNovicell/Models/Game.cs
+++ MVCNovicell/MVCNovicell/Models/Game.cs
@@ -10,18 +10,21 @@ namespace MVCNovicell.Models
     {
         public int Id { get; set; }
 
+        [Display(Name = "Title", ResourceType = typeof(Resources.language))]
         [StringLength(35, MinimumLength =1)]
         [Required]
         public string Title { get; set; }
 
-        [Display(Name = "Release Date")]
+        [Display(Name = "ReleaseDate", ResourceType = typeof(Resources.language))]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
+        [Display(Name = "Genre", ResourceType = typeof(Resources.language))]
         [RegularExpression(@"^[\w\d\s\.\\\*]*$"), StringLength(20)]
         [Required]
         public String Genre { get; set; }
 
+        [Display(Name = "Score", ResourceType = typeof(Resources.language))]
         [Column(TypeName = "decimal(18, 2)"), StringLength(5)]
         [Required]
         public decimal Score { get; set; }
